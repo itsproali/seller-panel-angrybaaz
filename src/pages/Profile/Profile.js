e8c40b901ac7100643fc3d60fa8ee95bba8345dd
Mohammad Ali <itsproali@gmail.com>
2022-11-29 21:04:08 +0600

    🔥 Profile Updated

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,6 +3,33 @@ import lamp from "../../assets/lamp.jpg";
 import styles from "../../styles/Profile.module.css";
 
 const Profile = () => {
+  const printingTypes = [
+    {
+      text: "Sublimation",
+      title: "Sublimation Printing",
+      img: "https://media.s-bol.com/RPMQon8jv23K/550x547.jpg",
+    },
+    {
+      text: "DTG",
+      title: "Direct to Garments Printing",
+      img: "https://i.etsystatic.com/11277991/r/il/b28393/1969500577/il_fullxfull.1969500577_qvb0.jpg",
+    },
+    {
+      text: "Plastisol",
+      title: "Indirect Screen Printing",
+      img: "https://png.pngitem.com/pimgs/s/377-3775707_plastisol-ink-t-shirt-hd-png-download.png",
+    },
+    {
+      text: "Vinyl",
+      title: "Heat Transfer Vinyl",
+      img: "https://images.jdmagicbox.com/quickquotes/images_main/vinyl-t-shirt-printing-services-381904221-b449s.jpg",
+    },
+    {
+      text: "Heat-press",
+      title: "Heat Press Printing",
+      img: "https://cdn.shopify.com/s/files/1/0070/7032/files/tshirt-business8-min_838a2996-f195-4c1d-a4d1-034bc0a9a095.jpg",
+    },
+  ];
   return (
     <div className={styles.profile}>
       <div className={styles.banner_img}>
@@ -11,6 +38,7 @@ const Profile = () => {
       </div>
 
       <div className={styles.info_container}>
+        {/* Personal Information */}
         <div className={styles.info_section}>
           <h3>Personal Information</h3>
           <div className={styles.input_container}>
@@ -21,6 +49,7 @@ const Profile = () => {
                 name="name"
                 id="name"
                 defaultValue="Mohammad Ali"
+                disabled
               />
             </div>
             <div className={styles.input_field}>
@@ -44,6 +73,8 @@ const Profile = () => {
             </div>
           </div>
         </div>
+
+        {/* Store Information */}
         <div className={styles.info_section}>
           <h3>Store Information</h3>
           <div className={styles.input_container}>
@@ -71,12 +102,26 @@ const Profile = () => {
             </div>
           </div>
         </div>
+
+        {/* Printing Types */}
         <div className={styles.info_section}>
           <div className="d-flex align-items-center justify-content-between">
             <h3>Printing Types</h3>
             <button className={styles.add_printing_type}>
               Add More Printing Type
             </button>
+          </div>
+
+          <div className="row row-cols-2 row-cols-md-4 row-cols-lg-5 g-2 mt-4">
+            {printingTypes.map((item, i) => (
+              <div key={i} title={item.title} className={styles.printing_item}>
+                <div className="">
+                  <img src={item.img} alt={item.text} className={`img-fluid`} />
+                  <p>{item.text}</p>
+                </div>
+                <div className={styles.divider}></div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
